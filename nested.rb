3a19d105c4867f0a215af628e60a3247e8269531
Neil <zooming.in@hotmail.com>
2021-07-10 22:02:03 +1000

    updates

--- nested.rb
+++ nested.rb
@@ -16,6 +16,8 @@ def hopper
       }
     }
 
+    programmer_hash[:grace_hopper]
+
 end
 
 def alan_kay_is_known_for
@@ -37,6 +39,8 @@ def alan_kay_is_known_for
       }
     }
 
+    programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -82,6 +88,9 @@ def adding_matz
       }
     }
 
+    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+    programmer_hash
+
 end
 
 def changing_alan
@@ -104,6 +113,9 @@ def changing_alan
       }
     }
 
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
+
 end
 
 def adding_to_dennis
@@ -125,5 +137,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+
+    programmer_hash[:dennis_ritchie][:languages] = programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash
 
 end
